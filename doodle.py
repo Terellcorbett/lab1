a8fc010b18ae3c6f8f993d347794a04b72daf097
Terellcorbett <47115513+Terellcorbett@users.noreply.github.com>
2019-02-23 15:32:14 -0500

    the drawling didnt come out as good as i wanted it too but its supposed to be a man with no legs climbing a rope

---
+++ doodle.py
@@ -0,0 +1,20 @@
+size(320,568)
+
+## begin program here.
+## see http://cooplogic.com/cheyney/procgraphics/
+## for the list of implemented functions
+background('red')
+fill(100)
+stroke('0')
+strokeWeight(3)
+ellipse(130,100,75,75)
+rectMode(CENTER)
+rect(130,200,50,200)
+line(130,400,130,5)
+stroke('blue')
+line(15,100,100,180)
+triangle(130,100,120,100,205,205)
+fill("green")
+stroke()
+text('one who never quits, can never lose',1,225)
+text('Terell corbett-lewis',1, 400)
